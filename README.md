8c28f5dbc32c4537e7a6d9159968fe86c7fa259e
SomdattaNag <nagsomdatta2@gmail.com>
2025-07-02 18:39:06 +0530

    added requirements, updated Readme, changed images

--- README.md
+++ README.md
@@ -17,13 +17,15 @@ an email alert to authorities. Otherwise, a safe alarm is triggered, allowing th
 5. if a match is found a threat_alarm is triggered and an email is sent directly to the authorities notifying them of the potential threat.
 
 # Workflow:
-1. Dataset: The dataset consists of folders, each representing an individual who is a known threat, criminal or wanted. Each folder contains images of that individual for identification.
+1. Dataset: The dataset contains folders for each wanted individual. The folder contains images of that individual. Larger the no. of images, better the precision of the system. The system has been tested with personal images. For now, sample stock images are used to fill the folder.
 
 2. Face Encoding & Labeling: The system loads all images, extracts facial features using the face_recognition model , and encodes them into numerical vectors.It then assigns each encoding to the person’s name based on the folder structure.
 
-3. Real time face recognition: Scans the individuals' face using OpenCV. Detects and extracts faces in real time. Compares detected faces against stored encodings using distance metrics. If similarity is above a certain threshold (< 0.4), the person is identified.
+3. Real time face recognition: Scans the individuals face using OpenCV. Detects and extracts faces in real time. Compares detected faces against stored encodings using distance metrics. If similarity is above a certain threshold (< 0.4), the person is identified. The is intentionally set lower in order to avoid false positives. While false negatives can be handled with additional procedures, false positives can cause serious issues and are therefore minimized.
 
 4. Identity Confirmation:  The system takes 10 seconds to analyze and confirm the match. If the match remains consistent for 10 seconds and the alarm has not been trigerred for last 30 seconds, it triggers the alarm based on the match type: safe_alarm for "No match" and threat_alarm for "match".
 
 5. Email Notification: If a threat is identified and threat_alarm is trigerred an email is sent to authorities including the name, photo, time and Ip location of the individual. The authorities can be the organisation security, admin, police authorities, local authorities, public safety etc.
 
+# Note:
+This is a prototype/demo project created for educational purposes and is not deployed in any real-world scenario yet.
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def threat_alarm():
 
 #alarm if no match found
 def safe_alarm():
-    duration=500
+    duration= 500
     frequency=1000
     winsound.Beep(frequency,duration)
 
@@ -49,16 +49,16 @@ try:
         if not ret:
             break
         
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)  # Resize
+        small_frame =cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)  # Resize
         rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)  # rgb-format
         
-        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_locations =face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         curr_names = []
         
         for face_encoding, face_location in zip(face_encodings, face_locations):
-            face_distances = face_recognition.face_distance(face_encode, face_encoding)
+            face_distances =face_recognition.face_distance(face_encode, face_encoding)
             best_match_index = np.argmin(face_distances)
             
             name = "No match"
@@ -66,8 +66,8 @@ try:
                 name = face_name[best_match_index]
             curr_names.append(name)
 
-            top, right, bottom, left = [coord * 4 for coord in face_location]
-            color = (0, 255, 0) if name != "No match" else (0, 0, 255)
+            top, right, bottom, left =[coord * 4 for coord in face_location]
+            color = (0, 255, 0) if name!= "No match" else (0, 0, 255)
             cv2.rectangle(frame, (left, top), (right, bottom), color, 2)
             cv2.putText(frame, name, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
         
@@ -77,21 +77,23 @@ try:
         
         for name in detected_now:
             if name not in detection_time:
+                
                 #first detection
                 detection_time[name] = curr_time
                 last_alarmed[name] = 0  
-        
+
+                
+
         for name in detected_now:
             scan_time= curr_time - detection_time[name]
             if scan_time>= 10 and (curr_time - last_alarmed.get(name, 0)) >= 30:
                 if name!="No match":
                     threat_alarm()
                     send_email(name,frame)
-                    
                 else:
                     safe_alarm()
                 last_alarmed[name] = curr_time
-        
+
         for name in list(detection_time.keys()):
             if name not in detected_now:
                 del detection_time[name]
